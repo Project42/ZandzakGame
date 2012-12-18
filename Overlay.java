b79f51a280b11804e26b3204adb6393d7ff7bb7e
Deborah Gaanderse <housefreaks@hotmail.com>
2012-12-18 14:22:01 +0100

    Overlay class

---
+++ Overlay.java
@@ -0,0 +1,12 @@
+import greenfoot.*;
+
+/**
+ * Overlay voor de menubalk.
+ * Wordt getoond als er een zak wordt geselecteerd.
+ * 
+ * @author Project 42
+ * @version 18 December
+ */
+
+public class Overlay extends Actor {
+}
