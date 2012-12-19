1ebf7ee6a2042b3deae2d9e525cb26b31d5881b0
Radek Slupik <daknok@icloud.com>
2012-12-19 16:32:44 +0100

    Make sure the player does not create multiple worlds

--- FloodWorldMenu.java
+++ FloodWorldMenu.java
@@ -9,14 +9,17 @@ public class FloodWorldMenu extends World {
     public void act() {
         if (Greenfoot.isKeyDown("1")) {
             Greenfoot.setWorld(new FloodWorld(Player.PlayerType.CITIZEN));
+            return;
         }
 
         if (Greenfoot.isKeyDown("2")) {
             Greenfoot.setWorld(new FloodWorld(Player.PlayerType.POLICE));
+            return;
         }
 
         if (Greenfoot.isKeyDown("3")) {
             Greenfoot.setWorld(new FloodWorld(Player.PlayerType.GENIUS));
+            return;
         }
     }
 }
