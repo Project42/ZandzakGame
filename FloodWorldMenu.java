380c07292c09508898ec8fd506c88756e594a30c
Deborah Gaanderse <housefreaks@hotmail.com>
2013-01-25 02:54:32 +0100

    Game 1 menu

--- FloodWorldMenu.java
+++ FloodWorldMenu.java
@@ -1,26 +1,34 @@
 import greenfoot.*;
 
 public class FloodWorldMenu extends World {
+    private boolean nextMenu = false;
     public FloodWorldMenu() {
         super(80, 80, 10);
-        setBackground("achtergrondSelectie.png");
+        setBackground("sandbaggame_menu.png");
     }
 
     @Override
     public void act() {
-        if (Greenfoot.isKeyDown("1")) {
-            Greenfoot.setWorld(new FloodWorld(Player.PlayerType.POLICE));
-            return;
+        if (Greenfoot.isKeyDown("enter")) {
+            setBackground("achtergrondSelectie.png");
+            nextMenu = true;
         }
-
-        if (Greenfoot.isKeyDown("2")) {
-            Greenfoot.setWorld(new FloodWorld(Player.PlayerType.GENIUS));
-            return;
-        }
-
-        if (Greenfoot.isKeyDown("3")) {
-            Greenfoot.setWorld(new FloodWorld(Player.PlayerType.CITIZEN));
-            return;
+        
+        if (nextMenu == true) {
+            if (Greenfoot.isKeyDown("1")) {
+                Greenfoot.setWorld(new FloodWorld(Player.PlayerType.POLICE));
+                return;
+            }
+    
+            if (Greenfoot.isKeyDown("2")) {
+                Greenfoot.setWorld(new FloodWorld(Player.PlayerType.GENIUS));
+                return;
+            }
+    
+            if (Greenfoot.isKeyDown("3")) {
+                Greenfoot.setWorld(new FloodWorld(Player.PlayerType.CITIZEN));
+                return;
+            }
         }
     }
 }
